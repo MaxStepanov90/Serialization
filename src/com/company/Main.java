3623e0e213de7ff5073aac862fa7e0ecccbfb49d
Max Stepanov <mu5cool@yandex.ru>
2020-03-11 22:46:06 +0300

    Commit1

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String fileName = "people.dat";
+
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Mike", 25, 180.0, false));
+        people.add(new Person("Frank", 45, 200.0, true));
+
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName));
+            oos.writeObject(people);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ArrayList<Person>newPeople = new ArrayList<>();
+
+
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));
+          newPeople = ((ArrayList<Person>) ois.readObject());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (Person person:newPeople){
+            System.out.println(person);
+        }
+    }
+}
---
+++ src/com/company/Person.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    private String name;
+    private int age;
+    private double height;
+    private transient boolean married;
+
+    public Person(String name, int age, double height, boolean married) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.married = married;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", married=" + married +
+                '}';
+    }
+}
